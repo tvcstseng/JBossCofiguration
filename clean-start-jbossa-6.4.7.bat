19896e65b7d54f0a9de9aef400317844b8211ff2
Timothy Tseng <timothy.tseng@tno.nl>
2016-04-28 15:52:26 +0200

    JBoss patched to 6.4.7

---
+++ clean-start-jbossa-6.4.7.bat
@@ -0,0 +1,21 @@
+REM ******************************************
+REM *** JBoss EAP 6.4.7 Preparation script ***
+REM ******************************************
+
+set "ROOT=C:\Development\Tools\new_jboss"
+set "JBOSS_HOME=%ROOT%\jboss-eap-6.4.7"
+set "STANDALONE=%JBOSS_HOME%\standalone"
+set "TMP=%STANDALONE%\tmp"
+set "DATA=%STANDALONE%\data"
+set "LOG=%STANDALONE%\log"
+set "DEPLOYMENTS=%STANDALONE%\deployments"
+
+@RD /S /Q %DATA%
+@RD /S /Q %TMP%
+
+DEL "%LOG%\*.log" /a /s
+DEL "%DEPLOYMENTS%\*.deployed" /a /s
+DEL "%DEPLOYMENTS%\*.failed" /a /s
+DEL "%DEPLOYMENTS%\*.isdeploying" /a /s
+
+CALL %ROOT%\start-jbossa-6.4.7.bat
